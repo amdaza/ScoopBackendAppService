968092d63d94a26aa485b180fda233a96884621b
amdaza <amdazacastillo@gmail.com>
2016-11-08 21:08:39 +0100

    Try upsert Authors

--- tables/Authors.js
+++ tables/Authors.js
@@ -6,3 +6,67 @@ var table = module.exports = require('azure-mobile-apps').table();
 // });
 
 // table.read.use(customMiddleware, table.operation);
+
+/*
+table.insert(function (context) {
+    context.item.userId = context.user.id;
+    return context.execute();
+});
+*/
+
+/*
+
+function insert(item, user, request) {
+    var table = tables.getTable('People');
+    table.where({ 
+       userId: user.userId
+    }).read({
+       success: upsertItem
+    });
+
+    function upsertItem(existingItems) {
+        if (existingItems.length === 0) {
+            request.execute();
+        } else {
+        item.id = existingItems[0].id;
+          table.update(item, {
+            success: function(updatedItem) {
+                request.respond(200, updatedItem)
+            }
+          });
+        }
+     }
+ }
+ */
+
+
+// CHANGE TO AUTHOR UPSERT
+
+ table.insert(function (context) {
+ 	var authorId = context.user.id;
+
+    //var table = tables.getTable('Authors');
+    table.where({ 
+       userId: authorId
+    }).read({
+       success: upsertItem
+    });
+
+    function upsertItem(existingItems) {
+        if (existingItems.length === 0) {
+        	// Insert author ...
+            request.execute();
+
+        } else {
+        	// Update author
+        	item.id = existingItems[0].id;
+	          table.update(item, {
+	            success: function(updatedItem) {
+                	request.respond(200, updatedItem)
+            	}
+        	});
+        }
+     }
+});
+
+ 
