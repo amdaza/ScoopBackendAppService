a781e049282b6429351234c1a27370a78baf5d63
amdaza <amdazacastillo@gmail.com>
2016-11-13 22:15:59 +0100

    Try to fix upsert

--- tables/Authors.js
+++ tables/Authors.js
@@ -52,27 +52,35 @@ table.insert(function (context) {
  	var authorId = context.user.id;
 
     //var table = tables.getTable('Authors');
+
+    var query = new azure.TableQuery()
+    .select(['userId'])
+    .top(1)
+    .where('userId eq ?', authorId);
+
+/*
     table.where({ 
        userId: authorId
     }).read({
        success: upsertItem
     });
-
-    function upsertItem(existingItems) {
-        if (existingItems.length === 0) {
+*/
+    //function upsertItem(existingItems) {
+        if (query.length === 0) {
         	// Insert author ...
-            request.execute();
+            return context.execute();
 
         } else {
         	// Update author
         	item.id = existingItems[0].id;
 	          table.update(item, {
 	            success: function(updatedItem) {
-                	request.respond(200, updatedItem)
+                	context.respond(200, updatedItem)
             	}
         	});
         }
-     }
+   //  }
+     
 });
 
  
